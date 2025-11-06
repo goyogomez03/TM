483a4c6f72a4d353cd156cadb73a19f5df5acde6
goyogomez03 <goyogomez03@gmail.com>
2025-11-06 14:56:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+#from PIL import Image
 from PIL import Image as Image, ImageOps as ImagOps
 from keras.models import load_model
 import paho.mqtt.client as paho
@@ -8,19 +9,20 @@ import time
 import json
 import platform
 
-def on_publish(client,userdata,result):             
+import platform
+def on_publish(client,userdata,result):             #create function for callback
     print("el dato ha sido publicado \n")
     pass
 
 def on_message(client, userdata, message):
     global message_received
     time.sleep(2)
-    message_received = str(message.payload.decode("utf-8"))
+    message_received=str(message.payload.decode("utf-8"))
     st.write(message_received)
 
-broker = "broker.mqttdashboard.com"
-port = 1883
-client1 = paho.Client("grego")
+broker="broker.mqttdashboard.com"
+port=1883
+client1= paho.Client("grego")
 client1.on_message = on_message
 
 # Fondo blanco
@@ -33,7 +35,7 @@ page_bg_img = """
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Muestra la versión de Python
+# Muestra la versión de Python junto con detalles adicionales
 st.write("Versión de Python:", platform.python_version())
 
 model = load_model('keras_model.h5')
@@ -49,31 +51,41 @@ with st.sidebar:
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+   #To read image file buffer as a PIL Image:
     img = Image.open(img_file_buffer)
 
     newsize = (224, 224)
     img = img.resize(newsize)
+    # To convert PIL Image to numpy array:
     img_array = np.array(img)
 
+    # Normalize the image
     normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
     data[0] = normalized_image_array
 
+    # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0] > 0.5:
-        st.header('enciende luz, con Probabilidad: ' + str(prediction[0][0]))
-        act1 = "ON"
-        client1 = paho.Client("grego")
-        client1.on_publish = on_publish
-        client1.connect(broker, port)
-        message = json.dumps({"Act1": act1})
-        ret = client1.publish("gregoriomensaje", message)
-    if prediction[0][1] > 0.5:
-        st.header('Apaga luz, con Probabilidad: ' + str(prediction[0][1]))
-        act1 = "OFF"
-        client1 = paho.Client("grego")
-        client1.on_publish = on_publish
-        client1.connect(broker, port)
-        message = json.dumps({"Act1": act1})
-        ret = client1.publish("gregoriomensaje", message)
+    if prediction[0][0]>0.5:
+      st.header('enciende luz, con Probabilidad: '+str( prediction[0][0]) )
+      act1="ON"
+      client1= paho.Client("grego")                           
+      client1.on_publish = on_publish                          
+      client1.connect(broker,port)  
+      message =json.dumps({"Act1":act1})
+      ret= client1.publish("gregoriomensaje", message)
+    if prediction[0][1]>0.5:
+      st.header('Apaga luz, con Probabilidad: '+str( prediction[0][1]))
+      act1="OFF"
+      client1= paho.Client("grego")                           
+      client1.on_publish = on_publish                          
+      client1.connect(broker,port)  
+      message =json.dumps({"Act1":act1})
+      ret= client1.publish("gregoriomensaje", message)
+    #if prediction[0][2]>0.5:
+    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+
+
